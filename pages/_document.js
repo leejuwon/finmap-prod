db1ce0aa770775bc1c0e488b732da0ec84fe09bf
leejuwon <ljw2061@gmail.com>
2025-11-12 15:19:58 +0900

    9

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,8 @@ class MyDocument extends Document {
   render(){
     return (
       <Html lang="ko">
-        <Head>          
+        <Head> 
+          <meta name="google-site-verification" content="8FhqQNDjbZ-QpdePXdPiCR_VJwQstaK-tbuYIlXxs_A" />         
         </Head>
         <body><Main/><NextScript/></body>
       </Html>
