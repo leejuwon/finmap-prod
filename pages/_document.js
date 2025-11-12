3b2bb490bfe4916da0ac7760a935cf07b263390e
leejuwon <ljw2061@gmail.com>
2025-11-12 14:44:29 +0900

    6

--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,18 @@ class MyDocument extends Document {
       <Html lang="ko">
         <Head>
           {process.env.NEXT_PUBLIC_GA_ID && (
-            <>
-              <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></script>
-              <script dangerouslySetInnerHTML={{__html:`
-                window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}}/>
-            </>
-          )}
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></script>
+            <script dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `
+            }}/>
+          </>
+        )}
         </Head>
         <body><Main/><NextScript/></body>
       </Html>
